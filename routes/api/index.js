1a073468055783ac3520e1c4853f310827a5fd3d
Lyle Denman <denmanlp@gmail.com>
2017-07-07 10:31:50 -0500

    Add Error routing

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,3 +1,20 @@
 var router = require('express').Router();
 
+router.use('/', require('./users'));
+
+router.use(function(err, req, res, next) {
+  if(err.name === 'ValidationError') {
+    return res.status(422).json({
+      errors: Object.keys(err.errors).reduce(function(errors, key) {
+
+        errors[key] = err.errors[key].message;
+
+        return errors;
+      }, {}) // this syntax allows for returning a cumulative object, how?
+    });
+  }
+
+	return next(err);
+});
+
 module.exports = router;
